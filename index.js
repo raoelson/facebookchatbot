d94f01ae934ad133d69d71fe129f5383ce8a6d9d
raoelson <raoelsonmaoris@gmail.com>
2017-08-17 17:38:06 +0200

    chat

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.listen(app.get('port'), function() {
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging_events
     for (let i = 0; i < messaging_events.length; i++) {
-        let event = req.body.entry[0].messaging_events[i]
+        let event = messaging_events[i]
         let sender = event.sender.id
         if (event.message && event.message.text) {
             let text = event.message.text
