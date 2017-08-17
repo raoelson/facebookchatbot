e3d30f7203a8dd0aca12ec2b88fb8a3aecc2b856
raoelson <raoelsonmaoris@gmail.com>
2017-08-17 17:46:43 +0200

    new add

--- index.js
+++ index.js
@@ -33,20 +33,21 @@ app.listen(app.get('port'), function() {
 // End Point
 
 app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging_events
+    let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
-        let event = messaging_events[i]
-        let sender = event.sender.id      
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
         if (event.message && event.message.text) {
             let text = event.message.text
-            if (text === 'salut') {
-                sendGenericMessage(sender)
+            if (text === 'Generic'){ 
+                console.log("welcome to chatbot")
+                //sendGenericMessage(sender)
                 continue
             }
-            sendTextMessage(sender, "Bot: " + text.substring(0, 200))
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
         }
         if (event.postback) {
-            text = JSON.stringify(event.postback)
+            let text = JSON.stringify(event.postback)
             sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
             continue
         }
@@ -83,64 +84,35 @@ function sendTextMessage(sender, text) {
 // Two cards.
 
 function sendGenericMessage(sender) {
-    messageData = {
+    let messageData = {
         "attachment": {
             "type": "template",
             "payload": {
                 "template_type": "generic",
                 "elements": [{
-                    "title": "Si vous souhaitez suivre",
-                    "subtitle": "social network",
-                    "image_url": "https://raw.githubusercontent.com/mlaidouni/FacebookBot/master/chatbot-facebook.jpg",
+                    "title": "First card",
+                    "subtitle": "Element #1 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
                     "buttons": [{
                         "type": "web_url",
-                        "url": "https://www.facebook.com/groups/1795624087378868/",
-                        "title": "FB Facebook Group"
+                        "url": "https://www.messenger.com",
+                        "title": "web url"
                     }, {
-                        "type": "web_url",
-                        "url": "https://www.youtube.com/channel/UCRV86HkxxSGI-whauOMkQdw",
-                        "title": "Me Suivre sur Youtube"
-                    },{
-                        "type": "web_url",
-                        "url": "https://twitter.com/elmehdimobi",
-                        "title": "Me Suivre sur Twitter"
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
                     }],
                 }, {
-                    "title": "Choses à savoir créer un bot",
-                    "subtitle": "Quelques questions sur notre bot",
-                    "image_url": "https://raw.githubusercontent.com/mlaidouni/FacebookBot/master/facebook-chatbots.png",
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
                     "buttons": [{
                         "type": "postback",
-                        "title": "C’est quoi un bot Facebook?",
-                        "payload": "Un bot conversationnel, c’est un logiciel informatique qui réalise des conversations automatisées. Un bot Facebook, c’est la même chose, mais sur un certain réseau social",
-                    },{
-                        "type": "postback",
-                        "title": "Quelles sont les entreprises qui ont essayé les bots ?",
-                        "payload": "Il s’agit de CNN, qui vous envoie un briefing par message chaque matin, comme une newsletter, quoi. En France, il y a notamment Skyscanner, Voyages-Sncf et KLM.",
-                    }, {
-                        "type": "postback",
-                        "title": "Comment créer votre bot Facebook?",
-                        "payload": "Il existe deux façons pour créer un bot facebook, soit d'utiliser une plateforme sans codage ou le créer en codage",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
                     }],
-                },  {
-                    "title": "Les outils nécessaires",
-                    "subtitle": "pour créer Facebook Messenger Bot",
-                    "image_url": "https://raw.githubusercontent.com/mlaidouni/FacebookBot/master/Facebook%20Messanger%20Bot.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "C'est quoi Node.js",
-                        "payload": "Node.js est une plateforme de développement Javascript. Ce n'est ni un serveur ,ni un Framework, c'est juste le langage Javascript avec des bibliothèques permettant de réaliser des actions comme écrire sur la sortie standard, ouvrir/fermer des connections réseaux ou encore créer un fichier.",
-                    },{
-                        "type": "postback",
-                        "title": "C'est quoi Heroku",
-                        "payload": "Heroku est une plateforme « Cloud » qui permet d’héberger, de développer tout type d’application. La plateforme appartient à Salesforce, gage de sécurité, de confidentialité et de haute performance.",
-                    }, {
-                        "type": "postback",
-                        "title": "C'est quoi GitHub",
-                        "payload": "GitHub est un site où n’importe qui peut déposer son projet web. Qu'il s'agit de quelques lignes de code pour une page modeste, ou d’une grosse application, les amateurs et professionnels ouvrent des comptes sur GitHub pour soumettre leur travail à l’appréciation de tous.",
-                    }],
-                }]  
-            } 
+                }]
+            }
         }
     }
     request({
